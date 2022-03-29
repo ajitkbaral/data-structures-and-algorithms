4454cc23ec6794429b594ca280aad07df46880b9
Ajit Kumar Baral <ajitkbaralofficial@gmail.com>
2022-03-29 22:00:54 +0545

    Bubble Sort

---
+++ Algorithms/2-Bubble Sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+
+    return my_list
+
+
+print(bubble_sort([4,2,6,5,1,3]))
