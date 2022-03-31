feef86e04efe882a69a0ce836770b1390c1e4732
Ajit Kumar Baral <ajitkbaralofficial@gmail.com>
2022-03-31 17:27:46 +0545

    Selection sort

---
+++ Algorithms/3-Selection Sort.py
@@ -0,0 +1,13 @@
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+print(selection_sort([4,2,6,5,1,3]))
