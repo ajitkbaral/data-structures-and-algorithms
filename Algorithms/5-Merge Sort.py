8b055ff1b666a12b7df96217e38fd0d525de9a3c
Ajit Kumar Baral <ajitkbaralofficial@gmail.com>
2022-03-31 20:56:18 +0545

    Merge sort

---
+++ Algorithms/5-Merge Sort.py
@@ -0,0 +1,32 @@
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list
+    mid = int(len(my_list)/2)
+    left = my_list[:mid]
+    right = my_list[mid:]
+    return merge(merge_sort(left), merge_sort(right))
+
+
+def merge(list1, list2):
+    combined = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+print("===========MERGE FUNCTION=============")
+print(merge([1,2,7,8], [3,4,5,6]))
+print("===========MERGE SORT=============")
+print(merge_sort([1,2,7,8,3,4,5,6]))
