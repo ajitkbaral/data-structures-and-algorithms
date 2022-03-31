2468b9248e7c4187e4157f202a728fae4a98658a
Ajit Kumar Baral <ajitkbaralofficial@gmail.com>
2022-03-31 18:33:55 +0545

    Insertion sort

---
+++ Algorithms/4-Insertion Sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([4,2,6,5,1,3]))
